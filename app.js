ca6b5865b1da7d41e1fa034fdd3c8587f8cb70ea
Cristian Luggren <cristianlugg@gmail.com>
2025-01-27 18:56:56 -0300

    Agrega función Sorteo

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let amigos = [];
-
+let numAmigo = 0;
 
 function agregarAmigo() {
-    nuevoAmigo = document.getElementById("amigo").value;
+    let nuevoAmigo = document.getElementById("amigo").value;
     if (nuevoAmigo !== '')        
         
         {
@@ -23,7 +23,7 @@ function limpiarCaja() {
  } 
 
  
- function actualizarLista() {
+function actualizarLista() {
     let lista = document.querySelector("#listaAmigos")
     lista.innerHTML = ""
     for (i in amigos) {
@@ -31,4 +31,13 @@ function limpiarCaja() {
         elementoLista.textContent = amigos[i]
         lista.appendChild(elementoLista);
     }
-}
+ }
+
+function sortearAmigo() {
+        let numAmigo = Math.floor(Math.random() * amigos.length)+1;
+        let sorteado = amigos[numAmigo];
+        let listaResultado = document.querySelector("#resultado")
+        let elementoLista2 = document.createElement("li2");
+        elementoLista2.textContent = sorteado
+        listaResultado.appendChild(elementoLista2)
+    }
