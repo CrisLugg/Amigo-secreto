7388c5a52699f41843fdf865ba63fc64b6de88dc
Cristian Luggren <cristianlugg@gmail.com>
2025-01-27 17:13:22 -0300

    Introduce las funciones: Actualizar, Agregar y Limpiar

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    nuevoAmigo = document.getElementById("amigo").value;
+    if (nuevoAmigo !== '')        
+        
+        {
+        if (amigos.includes(nuevoAmigo)){
+        return alert("Ya ingresaste ese nombre.");
+        } else {
+            amigos.push(nuevoAmigo);
+            limpiarCaja();
+            actualizarLista();
+            return nuevoAmigo} 
+        }
+        else 
+        alert("Por favor, inserte un nombre.")
+    }
+function limpiarCaja() {
+    let valorCaja = document.querySelector("#amigo");
+    valorCaja.value = ''
+ } 
+
+ 
+ function actualizarLista() {
+    let lista = document.querySelector("#listaAmigos")
+    lista.innerHTML = ""
+    for (i in amigos) {
+        let elementoLista = document.createElement("li");
+        elementoLista.textContent = amigos[i]
+        lista.appendChild(elementoLista);
+    }
+}
